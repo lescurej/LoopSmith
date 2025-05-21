4ce882595a2d0fb0aa2b1c5c838b23886649c0e1
Johan Lescure <johan.lescure@gmail.com>
2025-05-21 21:17:02 +0100

    Initial Commit

---
+++ LoopSmith/LoopSmithApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopSmithApp.swift
+//  LoopSmith
+//
+//  Created by Johan Lescure on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct LoopSmithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
